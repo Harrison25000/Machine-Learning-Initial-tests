27feb0448ce668714cd6785f3cf9db0f3c6d4793
Oliver <ofcharrison@gmail.com>
2019-04-17 14:39:20 +0100

    Iris challenge ML

---
+++ hello-world.py
@@ -0,0 +1,27 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+
+iris = load_iris()
+x = iris.data
+y = iris.target
+X_train, X_test, y_train, y_test=train_test_split(x,y,test_size=.2, random_state=42)
+
+
+clf = tree.DecisionTreeClassifier()
+clf.fit(X_train, y_train)
+prediction1 = clf.predict(X_test)
+print(accuracy_score(y_test, prediction1))
+
+clf2 = SVC()
+clf2.fit(X_train, y_train)
+prediction2 = clf2.predict(X_test)
+print(accuracy_score(y_test, prediction2))
+
+clf3 = RandomForestClassifier(n_estimators=1000, max_depth=2)
+clf3.fit(X_train, y_train)
+prediction3 = clf3.predict(X_test)
+print(accuracy_score(y_test, prediction3))
